777e9ae92960e59dc63d38d2576f6661f4dbd7c8
v.desyatov <v.desyatov@revo.ru>
2020-02-07 10:35:43 +0300

    Add model Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Подробнее о DSL, доступном в этом файле, смотрите http://rusrails.ru/rails-routing
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
