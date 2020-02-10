fcb50fd87b79f56f1704866db33b270b6dffa0ab
Vladimir19700101 <googlishe4ko@gmail.com>
2020-02-10 19:44:34 +0300

    Blog work! Make add,edit,delete comments and articles. It is finish! Completed.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,4 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  # Подробнее о DSL, доступном в этом файле, смотрите http://rusrails.ru/rails-routing
-  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
